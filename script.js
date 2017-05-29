7a2f1605cd836f1606070aff5e1ac4c6deaeac9b
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-05-29 11:28:16 +0200

    finished

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function placeX() {
 }
 
 var field = {
-	block1 : document.getElementbyId("block1").value,
-	block2 : document.getElementbyId("block2").value,
-	block3 : document.getElementbyId("block3").value,
-	block4 : document.getElementbyId("block4").value,
-	block5 : document.getElementbyId("block5").value,
-	block6 : document.getElementbyId("block6").value,
-	block7 : document.getElementbyId("block7").value,
-	block8 : document.getElementbyId("block8").value,
-	block9: document.getElementbyId("block9").value,
+	block1 : document.getElementbyId("#block1").value,
+	block2 : document.getElementbyId("#block2").value,
+	block3 : document.getElementbyId("#block3").value,
+	block4 : document.getElementbyId("#block4").value,
+	block5 : document.getElementbyId("#block5").value,
+	block6 : document.getElementbyId("#block6").value,
+	block7 : document.getElementbyId("#block7").value,
+	block8 : document.getElementbyId("#block8").value,
+	block9 : document.getElementbyId("#block9").value,
 };
 
 var json = JSON2.stringify(field);
