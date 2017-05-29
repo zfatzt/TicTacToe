70eff56a1413f64c7db54f9ff3883f7d19d4094b
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-05-29 12:18:24 +0200

    # WARNING: head commit changed in the meantime
    Xes working :D I like trains and trees and unicorn :D

--- index.php
+++ index.php
@@ -4,25 +4,24 @@
 <meta charset="ISO-8859-1">
 <link rel="stylesheet" type="text/css" href="style.css">
 <title>Tic Tac Toe</title>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
 	<h1>Tic Tac Toe</h1>
 	<div id="playField">
-		<div onclick="placeX()" class="block" id="block1"></div>
-		<div onclick="placeX()" class="block" id="block2"></div>
-		<div onclick="placeX()" class="block" id="block3"></div>
-		<div onclick="placeX()" class="block" id="block4"></div>
-		<div onclick="placeX()" class="block" id="block5"></div>
-		<div onclick="placeX()" class="block" id="block6"></div>
-		<div onclick="placeX()" class="block" id="block7"></div>
-		<div onclick="placeX()" class="block" id="block8"></div>
-		<div onclick="placeX()" class="block" id="block9"></div>
+		<div  class="block" id="block1"></div>
+		<div  class="block" id="block2"></div>
+		<div  class="block" id="block3"></div>
+		<div  class="block" id="block4"></div>
+		<div  class="block" id="block5"></div>
+		<div  class="block" id="block6"></div>
+		<div  class="block" id="block7"></div>
+		<div  class="block" id="block8"></div>
+		<div  class="block" id="block9"></div>
 	</div>
 	<h2>Player 0 won !</h2>
 </body>
-<script type="text/javascript" src="script.js">
-</script>
 </html>
 
 
--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-function placeX() {
-	document.getElementById("setX").innerHTML = "X"
-}
-
-var field = {
-	block1 : document.getElementbyId("#block1").value,
-	block2 : document.getElementbyId("#block2").value,
-	block3 : document.getElementbyId("#block3").value,
-	block4 : document.getElementbyId("#block4").value,
-	block5 : document.getElementbyId("#block5").value,
-	block6 : document.getElementbyId("#block6").value,
-	block7 : document.getElementbyId("#block7").value,
-	block8 : document.getElementbyId("#block8").value,
-	block9 : document.getElementbyId("#block9").value,
-};
-
-var json = JSON2.stringify(field);
-
-$.ajax({
-	type : "POST",
-	url : "TelephoneNumbers.aspx/DeleteNumber",
-	data : json,
-	contentType : "application/json; charset=utf-8",
-	dataType : "json",
-	success : function(msg) {
-		alert('In Ajax');
-	}
+$(document).ready(function(){
+    $(".block").click(function(){
+        $(this).append("X");
+    });
 });
 
-
-function checkFor3XInRow(){
-	if field.block1 == "X" && field.block2 == "X" && field.block3 == "X" || field.block1 == "x" && field.block4 == "X" && field.block7 == "X" || field.block2 == "X" && field.block5 == "X" &&field.block8 == "X" || field.block3 == "X" && field.block6 == "X" && field.block9 == "X",
-}
+//var field = {
+//	block1 : document.getElementbyId("#block1").value,
+//	block2 : document.getElementbyId("#block2").value,
+//	block3 : document.getElementbyId("#block3").value,
+//	block4 : document.getElementbyId("#block4").value,
+//	block5 : document.getElementbyId("#block5").value,
+//	block6 : document.getElementbyId("#block6").value,
+//	block7 : document.getElementbyId("#block7").value,
+//	block8 : document.getElementbyId("#block8").value,
+//	block9 : document.getElementbyId("#block9").value,
+//};
+//
+//var json = JSON2.stringify(field);
+//
+//$.ajax({
+//	type : "POST",
+//	url : "TelephoneNumbers.aspx/DeleteNumber",
+//	data : json,
+//	contentType : "application/json; charset=utf-8",
+//	dataType : "json",
+//	success : function(msg) {
+//		alert('In Ajax');
+//	}
+//});
+//
+//
+//function checkFor3XInRow(){
+//	if field.block1 == "X" && field.block2 == "X" && field.block3 == "X" || field.block1 == "x" && field.block4 == "X" && field.block7 == "X" || field.block2 == "X" && field.block5 == "X" &&field.block8 == "X" || field.block3 == "X" && field.block6 == "X" && field.block9 == "X",
+//}
