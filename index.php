8a51d68c36aa1e8c4a3de61b809d6832fabe2073
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-05-29 10:32:13 +0200

    da

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="style.css">
 <title>Tic Tac Toe</title>
+
 </head>
 <body>
-<h1>HELLO</h1>
+	<h1>Tic Tac Toe</h1>
+	<div id="playField">
+		<div class="block" id="block1"></div>
+		<div class="block" id="block2"></div>
+		<div class="block" id="block3"></div>
+		<div class="block" id="block4"></div>
+		<div class="block" id="block5"></div>
+		<div class="block" id="block6"></div>
+		<div class="block" id="block7"></div>
+		<div class="block" id="block8"></div>
+		<div class="block" id="block9"></div>
+	</div>
 </body>
 </html>
 
