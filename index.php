221ec0d41f1fc2c4b548973cc11191bb1dc73d64
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-05-29 10:58:16 +0200

    los commitos

--- index.php
+++ index.php
@@ -9,18 +9,22 @@
 <body>
 	<h1>Tic Tac Toe</h1>
 	<div id="playField">
-		<div class="block" id="block1"></div>
-		<div class="block" id="block2"></div>
-		<div class="block" id="block3"></div>
-		<div class="block" id="block4"></div>
-		<div class="block" id="block5"></div>
-		<div class="block" id="block6"></div>
-		<div class="block" id="block7"></div>
-		<div class="block" id="block8"></div>
-		<div class="block" id="block9"></div>
+		<div onclick="placeX()" class="block" id="block1"></div>
+		<div onclick="placeX()" class="block" id="block2"></div>
+		<div onclick="placeX()" class="block" id="block3"></div>
+		<div onclick="placeX()" class="block" id="block4"></div>
+		<div onclick="placeX()" class="block" id="block5"></div>
+		<div onclick="placeX()" class="block" id="block6"></div>
+		<div onclick="placeX()" class="block" id="block7"></div>
+		<div onclick="placeX()" class="block" id="block8"></div>
+		<div onclick="placeX()" class="block" id="block9"></div>
 	</div>
+	<h2>Player 0 won !</h2>
 </body>
+<script type="text/javascript" src="script.js">
+</script>
 </html>
+
 
 <?php
 ?>
